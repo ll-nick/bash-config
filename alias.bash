e014192f8f5370d82aaf6b8492cdb32abd23ec7a
Nick Le Large <nick@le-large.com>
2024-04-02 20:24:24 +0200

    Add ansible alias

--- alias.bash
+++ alias.bash
@@ -1,4 +1,5 @@
 alias auau="sudo apt update && sudo apt upgrade"
 alias coredumps="ls -d /var/lib/apport/coredump/*"
 alias screencastkeys="screenkey --no-detach -m --scr 1"
+alias update-config="ansible-pull -U https://github.com/ll-nick/ansible-config.git"
 
