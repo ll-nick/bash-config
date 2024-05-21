186273a2ba46dac209736614ec211413ddb86364
Nick Le Large <gitlab@le-large.com>
2024-05-22 00:01:24 +0200

    Add alias for privileged ansible update

--- alias.bash
+++ alias.bash
@@ -2,4 +2,5 @@ alias auau="sudo apt update && sudo apt upgrade"
 alias coredumps="ls -d /var/lib/apport/coredump/*"
 alias screencastkeys="screenkey --no-detach -m --scr 1"
 alias update-config="ansible-pull -U https://github.com/ll-nick/ansible-config.git"
+alias update-config-privileged="ansible-pull -U https://github.com/ll-nick/ansible-config.git --tags all,privileged -K"
 
