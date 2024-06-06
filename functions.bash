a87f03f796003fbb3f07c4baa1917b116f42fecd
Nick Le Large <nick.lelarge@kit.edu>
2024-06-06 16:34:10 +0200

    Add functions to switch git repo from/to https/ssh

--- functions.bash
+++ functions.bash
@@ -1,11 +1,31 @@
 open() { for f in "$@"; do xdg-open "$f"; done; }
 
-connect_ros() {
+connect-ros() {
   if [ $# -ne 1 ]; then
     echo "Usage: connect_ros <hostname>"
     return 1
   fi
 
   export ROS_MASTER_URI="http://$1:11311"
+}
+
+git-ssh() {
+    local remote=${1:-origin}
+    local current_url
+    current_url=$(git remote get-url "$remote")
+    local new_url
+    new_url=$(echo "$current_url" | sed -E 's,^https://([^/]*)/(.*)$,git@\1:\2,')
+    git remote set-url "$remote" "$new_url"
+    echo "Changed remote $remote to $new_url"
+}
+
+git-https() {
+    local remote=${1:-origin}
+    local current_url
+    current_url=$(git remote get-url "$remote")
+    local new_url
+    new_url=$(echo "$current_url" | sed -E 's,^git@([^:]*):/*(.*)$,https://\1/\2,')
+    git remote set-url "$remote" "$new_url"
+    echo "Changed remote $remote to $new_url"
 }
 
