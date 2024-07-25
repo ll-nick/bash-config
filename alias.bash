588313d920bde9e1c3a57941f43318b4bd61b0d3
Nick Le Large <nick.lelarge@kit.edu>
2024-07-25 13:28:40 +0200

    Add alias to easily enable coredumps

--- alias.bash
+++ alias.bash
@@ -1,5 +1,6 @@
 alias auau="sudo apt update && sudo apt upgrade"
 alias coredumps="ls -d /var/lib/apport/coredump/*"
+alias enable_coredumps="ulimit -c unlimited && echo 'core.%e.%p.%h.%t' | sudo tee /proc/sys/kernel/core_pattern"
 alias screencastkeys="screenkey --no-detach -m --scr 1"
 alias update-config="ansible-pull -U https://github.com/ll-nick/ansible-config.git"
 alias update-config-privileged="ansible-pull -U https://github.com/ll-nick/ansible-config.git --tags all,privileged -K"
